bebfa9263827d094cf58e90d8f141df38069b4de
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2023-04-15 19:01:37 +0300

    add lazy loading

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,7 +9,7 @@ function onCreateGalleryMarcup(galleryItems) {
         return `
         <li class="gallery__item">
             <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
+                <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
             </a>
         </li>
 `
